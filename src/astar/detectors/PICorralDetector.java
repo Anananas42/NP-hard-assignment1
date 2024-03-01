791749d67e2dffc1db7e4ffaae046c866be053c3
Miia Zhang <D.Zhang-16@student.tudelft.nl>
2024-03-01 23:29:07 +0100

    added corral detector

--- src/astar/detectors/PICorralDetector.java
+++ src/astar/detectors/PICorralDetector.java
@@ -1,5 +1,48 @@
 package astar.detectors;
 
+import astar.BoardCustom;
+import astar.actions.TTile;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PICorralDetector {
-    
+    // Tiles containing only walls and targets.
+    private Byte[][] tiles;
+
+    public void constructTiles(Byte[][] boardTiles) {
+        for (int i = 0; i < boardTiles.length; i++) {
+            for (int j = 0; j < boardTiles[0].length; j++) {
+                tiles[i][j] = boardTiles[i][j];
+
+            }
+        }
+    }
+
+    public void detect(BoardCustom board) {
+        int[] positions = board.positions;
+
+        // Construct boxes if they are not in the target places.
+        for(int i = 1; i < positions.length; i++) {
+            int x = board.getX(positions[i]);
+            int y = board.getY(positions[i]);
+            if(tiles[x][y]!=TTile.PLACE_FLAG) {
+                tiles[x][y] = TTile.BOX_FLAG;
+            }
+        }
+
+
+        Queue<Integer> queue = new LinkedList<>();
+
+
+        // Erase boxes from tiles.
+        for(int i = 1; i < positions.length; i++) {
+            int x = board.getX(positions[i]);
+            int y = board.getY(positions[i]);
+            if(tiles[x][y]==TTile.BOX_FLAG) {
+                tiles[x][y] = TTile.NONE_FLAG;
+            }
+        }
+    }
+
 }
